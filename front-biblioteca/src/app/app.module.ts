df478081a3fc22ecf002da985969b9457f74f4bd
Jonnatan <jrios328@gmail.com>
2019-11-09 11:41:34 -0500

    componente agregar libro

--- front-biblioteca/src/app/app.module.ts
+++ front-biblioteca/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AgregarLibroComponent } from './components/agregar-libro/agregar-libro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarLibroComponent
   ],
   imports: [
     BrowserModule,

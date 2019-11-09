d8004a0e823796184d28e14ba375917c19450365
juandavid <juandavid@acerdeb.acerhost>
2019-11-09 13:17:01 -0500

    Initial Commit Backend Biblioteca

---
+++ backend-biblioteca/src/main/java/com/ceibalab/backendbiblioteca/BackendBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.ceibalab.backendbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBibliotecaApplication.class, args);
+	}
+
+}

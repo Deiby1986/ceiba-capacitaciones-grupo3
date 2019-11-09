62796ed66a7ace0b91266b79d00667de149f78c0
Deiby Manzo <deiby1986@gmail.com>
2019-11-09 14:06:06 -0500

    front-end para agregar libro

--- front-biblioteca/src/app/app-routing.module.ts
+++ front-biblioteca/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AgregarLibroComponent } from './components/agregar-libro/agregar-libro.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"agregar-libro",component:AgregarLibroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-biblioteca/src/app/app.module.ts
+++ front-biblioteca/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AgregarLibroComponent } from './components/agregar-libro/agregar-libro.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { AgregarLibroComponent } from './components/agregar-libro/agregar-libro.
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

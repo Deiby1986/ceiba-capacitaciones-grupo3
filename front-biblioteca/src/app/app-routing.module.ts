4ae295e883cd37ae2515a7205042a0235330965c
Deiby Manzo <deiby1986@gmail.com>
2019-11-09 15:12:51 -0500

    front-end para agregar prestamo

--- front-biblioteca/src/app/app-routing.module.ts
+++ front-biblioteca/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AgregarPrestamoComponent } from './components/agregar-prestamo/agregar-prestamo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:"agregar-prestamo",component:AgregarPrestamoComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-biblioteca/src/app/app.module.ts
+++ front-biblioteca/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AgregarPrestamoComponent } from './components/agregar-prestamo/agregar-prestamo.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarPrestamoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
